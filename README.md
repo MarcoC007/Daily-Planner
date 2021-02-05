2fee2a3fb534aa0133407bac23859f425edcc03c
Marco Estrada <marco2110estrada@gmail.com>
2021-02-04 23:52:02 -0500

    adding new assignment

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Daily-Planner
-Daily-Planner
+## Info
+* This is a Daily Planner App that when each line is in the present time changes color to red, when is past is grey and green in future.
+
+## Screenshots
+
---
+++ script.js
@@ -0,0 +1,46 @@
+var currentDate = moment().format("dddd, MMMM Do");
+var currentTime = moment();
+var schedule = {};
+
+// display the current date 
+$('#currentDay').text(currentDate);
+
+// event listener for each button
+$(".saveBtn").on('click', function () {
+
+    var text = $(this).siblings(".description").val();
+ 
+    var time = $(this).parent().attr("id");
+
+    schedule[time] = text;
+
+    var scheduleS = JSON.stringify(schedule);
+
+    localStorage.setItem('schedule', scheduleS);
+
+    console.log('message');
+});
+
+
+
+
+// if local storage have data and Json parse will retrieve the data back to the textarea
+// var currentHour = JSON.parse(localStorage.getItem("schedule"));
+
+
+var storage = localStorage.getItem('schedule');
+
+if (storage) {
+    schedule = JSON.parse(storage);
+}
+else {
+    for (var i = 0; i <= 17; i++) {
+        schedule[i] = ""
+        console.log('');
+       
+    }
+}
+//this for loop is for change the textarea color when the time is passing
+
+}
+
